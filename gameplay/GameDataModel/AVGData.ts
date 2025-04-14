9f111a0fcfbb10983a00450ae709ed2dae8fc89e
JRframe <1042785075@qq.com>
2025-04-14 18:39:19 +0800

    add avalanche subnet support for custom vm integration

--- gameplay/GameDataModel/AVGData.ts
+++ gameplay/GameDataModel/AVGData.ts
@@ -3,13 +3,15 @@
 import { SerializeClass, SerializeData } from "../../../modules/base/SerializeClass";
 import { TimeUtility } from "../Utility/TimeUtility";
 
+/// AVG
 export class AVGData extends SerializeClass {
     __className:string = "AVGData";
+
+    // ===========  ===========
 
     @SerializeData() // id
     sceneEnterCount: Map<number,number> = new Map();
 
-    // ===========  ===========
     @SerializeData() // id+id
     EventStoryState: Map<string,boolean> = new Map();
     @SerializeData()
